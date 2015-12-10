35af652b7218427e988b2933315f7b63aa502a25
igrs <specialweekisno1@gmail.com>
2015-12-10 11:06:27 +0900

    add articles relation to schema

--- priv/repo/migrations/20151208052203_initial_schema.exs
+++ priv/repo/migrations/20151208052203_initial_schema.exs
@@ -2,14 +2,34 @@ defmodule TokailyPhoenix.Repo.Migrations.InitialSchema do
   use Ecto.Migration
 
   def change do
+
     create table(:users) do
       add :email,            :string, null: false
       add :crypted_password, :string, null: false
       add :name,             :string, null: true
+      timestamps
+    end
+    create unique_index(:users, [:email]) # Unique constraint
 
+    create table(:categories) do
+      add :name, :string, null: false
       timestamps
     end
 
-    create unique_index(:users, [:email])
+    create table(:articles) do
+      add :title,       :string,  null: false
+      add :body,        :string,  null: false
+      add :category_id, references(:categories)
+      timestamps
+    end
+    create index(:articles, [:category_id]) # Foreign key
+    create index(:articles, [:updated_at])  # Index
+
+    create table(:materials) do
+      add :memo, :string, null: true
+      add :path, :string, null: false
+      timestamps
+    end
+
   end
 end
